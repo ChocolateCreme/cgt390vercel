bed252faba157f629390a4231a9655f7e9d6bf5b
Avani Bhargava <bharga66@purdue.edu>
2026-03-24 19:33:47 -0400

    Lab16

--- app/page.js
+++ app/page.js
@@ -23,6 +23,66 @@ async function getData(title, search){
   return profiles ? profiles.profile : [];
 }
 
+export async function GET() {
+    const profiles = [
+        {id: 1, name: "Ava Lee", major: "CS", year: 2, gpa: 3.6},
+        {id: 2, name: "Ben Park", major: "CGT", year: 3, gpa: 3.2},
+    ]
+    return Response.json(profiles)
+}
+
+export async function POST (request) {
+    const body = await request.json();
+    const {name, major, year, gpa} = body;
+    const newUser = {id: Date.now()};
+
+    if (!name) {
+        return Response.json(
+            {error: 'Enter a name'},
+            {status: 400}
+        )
+    }
+
+    if (!major) {
+        return Response.json(
+            {error: 'Enter a major'},
+            {status: 400}
+        )
+    }
+
+    if (!year) {
+        return Response.json(
+            {error: 'Enter a year'},
+            {status: 400}
+        )
+    }
+
+    if (!gpa) {
+        return Response.json(
+            {error: 'Enter a gpa'},
+            {status: 400}
+        )
+    }
+    
+    return Response.json(
+        {message: 'Post Created!', data: {name, major, year, gpa}},
+        {status: 201}
+    )
+}
+
+export async function DELETE (request, {params}) {
+    const {id} = await params
+    return Response.json(
+        {message: 'Post Deleted'},
+        {status: 404}
+    )
+}
+
+export async function PUT(request, { params }) {
+    const { id } = await params
+    return Response.json({ id })
+}
+
 export default function Home() {
   return (
     <>
